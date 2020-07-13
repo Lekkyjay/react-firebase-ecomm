06b884fbf74d05e873bb1cdb2828a347d90e61ae
Lekkyjay <lekkyjayjay@yahoo.com>
2020-07-13 19:30:15 +0200

    Integrated firebase into application
    Implemented google signin functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
+import { auth } from './firebase/firebase.utils';
+
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  // let unsubscribeFromAuth = null
+
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged(authUser => {
+      authUser ? setCurrentUser(authUser) : setCurrentUser(null)
+    })
+    return () => {
+      unsubscribeFromAuth();
+    }
+  }, [])
+
+  console.log('currentUser:', currentUser);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header currentUser={currentUser} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyA2_kFJQjJNJ9l0osowOI37-3suNEHpaM4",
+  authDomain: "e-comm-52c4f.firebaseapp.com",
+  databaseURL: "https://e-comm-52c4f.firebaseio.com",
+  projectId: "e-comm-52c4f",
+  storageBucket: "e-comm-52c4f.appspot.com",
+  messagingSenderId: "575866999951",
+  appId: "1:575866999951:web:d72bb0c67ef1b2d0d5ccfa",
+  measurementId: "G-6RD0GX57EC"
+};
+
+firebase.initializeApp(config);
+
+// export const createUserProfileDocument = async (userAuth, additionalData) => {
+//   if (!userAuth) return;
+
+//   const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+//   const snapShot = await userRef.get();
+
+//   if (!snapShot.exists) {
+//     const { displayName, email } = userAuth;
+//     const createdAt = new Date();
+//     try {
+//       await userRef.set({
+//         displayName,
+//         email,
+//         createdAt,
+//         ...additionalData
+//       });
+//     } catch (error) {
+//       console.log('error creating user', error.message);
+//     }
+//   }
+
+//   return userRef;
+// };
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
